c66d973b03bb762912830402c36181eb4dd0cdd3
Ogemdi O <orjiogemdi@yahoo.com>
2021-06-04 01:57:43 -0400

    Carousel Indicators and Button

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(".carousel").carousel({interval: 2000, pause:"false"});
+    $(".carousel-inner").click(function(){
+        if($("#carouselButton").children("i").hasClass("fa-pause")){
+            $("#homeCarousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+            $("#carouselButton").removeClass("d-none");
+            $("#carouselButton").addClass("d-block");
+        } else {
+            $("#homeCarousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+            $("#carouselButton").removeClass("d-block");
+            $("#carouselButton").addClass("d-none");
+        }
+    });
+    $("#carouselButton").click(function(){
+        $("#homeCarousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+            $("#carouselButton").removeClass("d-block");
+            $("#carouselButton").addClass("d-none");
+    })
+});
